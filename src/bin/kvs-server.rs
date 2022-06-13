fa11dcf75909dbdefdf6de885d6ae804a09ddf96
Vifly <viflythink@gmail.com>
2022-06-13 23:20:21 +0800

    replace clap with argh to reduce project size

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,27 +1,52 @@
-extern crate clap;
 #[macro_use(o)]
 extern crate slog;
 #[macro_use]
 extern crate slog_scope;
 extern crate slog_term;
+extern crate strum;
 
 use std::net::SocketAddr;
 use std::process::exit;
 
-use clap::{App, crate_authors, crate_description, crate_version, load_yaml};
+use argh::FromArgs;
 use slog::{Drain, PushFnValue, PushFnValueSerializer, Record};
+use strum_macros;
 
 use kvs::KvsServer;
 
-fn main() {
-    let yaml = load_yaml!("server-cli.yml");
-    let m = App::from_yaml(yaml)
-        .version(crate_version!())
-        .about(crate_description!())
-        .author(crate_authors!())
-        .get_matches();
+#[derive(Debug, Eq, PartialEq, strum_macros::Display, strum_macros::EnumString)]
+#[strum(serialize_all = "snake_case")]
+enum Engine {
+    Kvs,
+    Sled,
+}
 
-    let addr = m.value_of("addr").unwrap_or("127.0.0.1:4000").to_string();
+#[derive(FromArgs)]
+/// Kvs server
+struct Args {
+    /// print version information
+    #[argh(switch, short = 'V')]
+    version: bool,
+
+    /// IP:port, used to connect server
+    #[argh(option)]
+    addr: Option<String>,
+
+    /// specify an engine [possible values: kvs, sled]
+    #[argh(option)]
+    engine: Option<Engine>,
+}
+
+
+fn main() {
+    let args: Args = argh::from_env();
+
+    if args.version {
+        println!("kvs-server {}", env!("CARGO_PKG_VERSION"));
+        exit(0);
+    }
+
+    let addr = args.addr.unwrap_or("127.0.0.1:4000".to_string());
     let socket_addr: SocketAddr = match addr.parse() {
         Ok(val) => val,
         Err(_e) => {
@@ -29,7 +54,16 @@ fn main() {
             exit(-1);
         }
     };
-    let engine = m.value_of("engine").unwrap_or("kvs").to_string();
+
+    let engine = match args.engine {
+        Some(engine_arg) => {
+            match engine_arg {
+                Engine::Kvs => Engine::Kvs.to_string(),
+                Engine::Sled => Engine::Sled.to_string()
+            }
+        }
+        None => "kvs".to_string()
+    };
 
     let plain = slog_term::PlainSyncDecorator::new(std::io::stderr());
     let logger = slog::Logger::root(
@@ -40,7 +74,7 @@ fn main() {
     );
     let _guard = slog_scope::set_global_logger(logger);
 
-    info!("Server version: {}", crate_version!());
+    info!("Server version: {}", env!("CARGO_PKG_VERSION"));
     info!("Run with {} engine", engine);
     info!("Listening on {}", addr);
 
